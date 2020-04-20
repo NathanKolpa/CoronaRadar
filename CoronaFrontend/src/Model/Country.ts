ab55e5bf8f15ab741aa70119d59fc205594f6ab5
michiellegters2 <michiellegters2@gmail.com>
2020-04-20 12:45:59 +0200

    Added front end model

---
+++ CoronaFrontend/src/Model/Country.ts
@@ -0,0 +1,24 @@
+class Country
+{
+    private provinceList : Array<Province> = new Array<Province>();
+
+    public constructor(private _name: string)
+    {
+       
+    }
+
+    public set name(name : string)
+    {
+        this._name = name;
+    }
+
+    public get name() : string
+    {
+        return this._name;
+    }
+
+    public addProvince(province : Province) : void
+    {
+        this.provinceList.push(province);
+    }
+}
---
+++ CoronaFrontend/src/Model/Province.ts
@@ -0,0 +1,47 @@
+class Province
+{
+    public constructor(private _name : string, private _infected: number, private _death: number, private _recovered: number)
+    {
+
+    }
+
+    public set name(name: string)
+    {
+        this._name = name;
+    }
+
+    public get name() : string
+    {
+        return this._name;
+    }
+
+    public set infected(infected: number)
+    {
+        this._infected = infected;
+    }
+
+    public get infected() : number
+    {
+        return this._infected;
+    }
+
+    public set death(death: number)
+    {
+        this._death = death;
+    }
+
+    public get death() : number
+    {
+        return this._death;
+    }
+
+    public set recovered(recovered: number)
+    {
+        this._recovered = recovered;
+    }
+
+    public get recovered() : number
+    {
+        return this._recovered;
+    }
+}
