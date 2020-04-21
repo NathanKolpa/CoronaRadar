521ddd329ffc777e03a8b1b90ee8a5faf433087b
Nathan Kolpa <nathan@kolpa.me>
2020-04-21 09:54:25 +0200

    use parcel instead of links

--- CoronaFrontend/src/app.ts
+++ CoronaFrontend/src/app.ts
@@ -1,1 +1,17 @@
-console.log('test');
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+	target: 'map',
+	layers: [
+		new TileLayer({
+			source: new OSM()
+		})
+	],
+	view: new View({
+		center: [0, 0],
+		zoom: 0
+	})
+});
