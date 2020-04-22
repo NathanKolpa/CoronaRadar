302fda4a342869087a675462569001ebc43e9994
michiellegters2 <michiellegters2@gmail.com>
2020-04-22 11:04:27 +0200

    added http

---
+++ CoronaBackend/src/main/java/webapi/web.java
@@ -0,0 +1,12 @@
+package webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class web
+{
+	public static void main(String[] args) {
+		SpringApplication.run(web.class, args);
+	}
+}
