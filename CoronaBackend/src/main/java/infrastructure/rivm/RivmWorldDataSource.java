106f53dda3260303167c42713cfa28ccf6cd1227
Nathan Kolpa <nathan@kolpa.me>
2020-04-23 11:04:26 +0200

    pronicemapper

---
+++ CoronaBackend/src/main/java/infrastructure/rivm/RivmWorldDataSource.java
@@ -0,0 +1,14 @@
+package infrastructure.rivm;
+
+import business.model.World;
+import business.worlddata.WorldDataSource;
+
+public class RivmWorldDataSource implements WorldDataSource
+{
+	@Override
+	public World getWorld() throws Exception
+	{
+		return null;
+	}
+
+}
