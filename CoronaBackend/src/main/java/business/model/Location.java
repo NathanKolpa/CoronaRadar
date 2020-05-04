521f700033d7bf46e3a07a377b4153d7083a877c
Nathan Kolpa <nathan@kolpa.me>
2020-05-04 13:04:37 +0200

    help

--- CoronaBackend/src/main/java/business/model/Location.java
+++ CoronaBackend/src/main/java/business/model/Location.java
@@ -5,7 +5,7 @@ public class Location {
     private double lat;
     private double lon;
 
-    public Location(double lon, double lat)
+    public Location(double lat, double lon)
     {
         this.lat = lat;
         this.lon = lon;
