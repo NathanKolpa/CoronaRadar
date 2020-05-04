e902c6899366c01dbed4525e1314a4f5fc4eb4c7
Nathan Kolpa <nathan@kolpa.me>
2020-05-04 12:45:32 +0200

    env

--- CoronaFrontend/app/app.ts
+++ CoronaFrontend/app/app.ts
@@ -2,13 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'ol/ol.css';
 
-import {IMap} from "./src/Core/Map/IMap";
-import {OpenlayersMap} from "./src/Infrastructure/Openlayers/OpenlayersMap";
-import {IConfig} from "./src/Core/Config/IConfig";
-import {ProcessConfig} from "./src/Infrastructure/Config/ProcessConfig";
-import {CoronaMap} from "./src/Core/CoronaMap";
-import {ApiWorldGetter} from "./src/Infrastructure/Api/ApiWorldGetter";
-import {Http} from "./src/Infrastructure/Api/Http";
+import {IMap} from "~src/Core/Map/IMap";
+import {OpenlayersMap} from "~src/Infrastructure/Openlayers/OpenlayersMap";
+import {IConfig} from "~src/Core/Config/IConfig";
+import {ProcessConfig} from "~src/Infrastructure/Config/ProcessConfig";
+import {CoronaMap} from "~src/Core/CoronaMap";
+import {ApiWorldGetter} from "~src/Infrastructure/Api/ApiWorldGetter";
+import {Http} from "~src/Infrastructure/Api/Http";
 
 const config: IConfig = new ProcessConfig();
 
--- CoronaFrontend/app/src/Infrastructure/Config/ProcessConfig.ts
+++ CoronaFrontend/app/src/Infrastructure/Config/ProcessConfig.ts
@@ -1,9 +1,9 @@
-import {IConfig} from "~Core/Config/IConfig";
+import {IConfig} from "~src/Core/Config/IConfig";
 
 export class ProcessConfig implements IConfig {
 	getApiOrigin(): string {
 		// @ts-ignore
-		return process.env.api;
+		return process.env.API;
 	}
 
 }
