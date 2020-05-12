42a6018c6fbb83efcbcd92e6c5fc26de5c1943c3
Nathan Kolpa <nathan@kolpa.me>
2020-05-12 08:57:34 +0200

    mapper

---
+++ CoronaBackend/src/main/java/infrastructure/rivm/CouncilLocationMapper.java
@@ -0,0 +1,428 @@
+package infrastructure.rivm;
+
+import business.model.Location;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CouncilLocationMapper
+{
+	private Map<String, Location> map = new HashMap<>();
+
+	public CouncilLocationMapper()
+	{
+		// Drenthe
+		map.put("Aa en Hunze", "drenthe");
+		map.put("Assen", "drenthe");
+		map.put("Borger-Odoorn", "drenthe");
+		map.put("Coevorden", "drenthe");
+		map.put("Emmen", "drenthe");
+		map.put("Hoogeveen", "drenthe");
+		map.put("Meppel", "drenthe");
+		map.put("Midden-Drenthe", "drenthe");
+		map.put("Noordenveld", "drenthe");
+		map.put("Tynaarlo", "drenthe");
+		map.put("Westerveld", "drenthe");
+		map.put("De Wolden", "drenthe");
+
+		//flevoland
+		map.put("Almere", "flevoland");
+		map.put("Dronten", "flevoland");
+		map.put("Lelystad", "flevoland");
+		map.put("Noordoostpolder", "flevoland");
+		map.put("Urk", "flevoland");
+		map.put("Zeewolde", "flevoland");
+
+		//Friesland
+		map.put("Achtkarspelen", "fiesland");
+		map.put("Ameland", "fiesland");
+		map.put("Dantumadiel", "fiesland");
+		map.put("De Fryske Marren", "fiesland");
+		map.put("Harlingen", "fiesland");
+		map.put("Heerenveen", "fiesland");
+		map.put("Leeuwarden", "fiesland");
+		map.put("Noardeast-FryslÃ¢n", "fiesland");
+		map.put("Ooststellingwerf", "fiesland");
+		map.put("Opsterland", "fiesland");
+		map.put("Schiermonnikoog", "fiesland");
+		map.put("Smallingerland", "fiesland");
+		map.put("SÃºdwest-FryslÃ¢n", "fiesland");
+		map.put("Terschelling", "fiesland");
+		map.put("Tytsjerksteradiel", "fiesland");
+		map.put("Vlieland", "fiesland");
+		map.put("Waadhoeke", "fiesland");
+		map.put("Weststellingwerf", "fiesland");
+
+		//flevoland
+		map.put("Almere", "flevoland");
+		map.put("Dronten", "flevoland");
+		map.put("Lelystad", "flevoland");
+		map.put("Noordoostpolder", "flevoland");
+		map.put("Urk", "flevoland");
+		map.put("Zeewolde", "flevoland");
+
+		//Friesland
+		map.put("Achtkarspelen", "fiesland");
+		map.put("Ameland", "fiesland");
+		map.put("Dantumadeel", "fiesland");
+		map.put("De friese meren", "fiesland");
+		map.put("Harlingen", "fiesland");
+		map.put("Heerenveen", "fiesland");
+		map.put("Leeuwarden", "fiesland");
+		map.put("Noardeast-Fryslând", "fiesland");
+		map.put("Ooststellingwerf", "fiesland");
+		map.put("Opsterland", "fiesland");
+		map.put("Schiermonnikook", "fiesland");
+		map.put("Smallingerland", "fiesland");
+		map.put("Súdwest-Fryslân", "fiesland");
+		map.put("Terschelling", "fiesland");
+		map.put("Tietjerksteradeel", "fiesland");
+		map.put("Vlieland", "fiesland");
+		map.put("Waadhoeke", "fiesland");
+		map.put("Weststellingwerf", "fiesland");
+
+		// Gelderland
+		map.put("Aalten", "gelderland");
+		map.put("Apeldoorn", "gelderland");
+		map.put("Arnhem", "gelderland");
+		map.put("Barneveld", "gelderland");
+		map.put("Berg en Dal", "gelderland");
+		map.put("Berkelland", "gelderland");
+		map.put("Beuningen", "gelderland");
+		map.put("Bronckhorst", "gelderland");
+		map.put("Brummen", "gelderland");
+		map.put("Buren", "gelderland");
+		map.put("Culemborg", "gelderland");
+		map.put("Doesburg", "gelderland");
+		map.put("Doetinchem", "gelderland");
+		map.put("Druten", "gelderland");
+		map.put("Duiven", "gelderland");
+		map.put("Ede", "gelderland");
+		map.put("Elburg", "gelderland");
+		map.put("Epe", "gelderland");
+		map.put("Ermelo", "gelderland");
+		map.put("Harderwijk", "gelderland");
+		map.put("Hattem", "gelderland");
+		map.put("Heerde", "gelderland");
+		map.put("Heumen", "gelderland");
+		map.put("Lingewaard", "gelderland");
+		map.put("Lochem", "gelderland");
+		map.put("Maasdriel", "gelderland");
+		map.put("Montferland", "gelderland");
+		map.put("Neder-Betuwe", "gelderland");
+		map.put("Nijkerk", "gelderland");
+		map.put("Nijmegen", "gelderland");
+		map.put("Nunspeet", "gelderland");
+		map.put("Oldebroek", "gelderland");
+		map.put("Oost Gelre", "gelderland");
+		map.put("Oude IJsselstreek", "gelderland");
+		map.put("Overbetuwe", "gelderland");
+		map.put("Putten", "gelderland");
+		map.put("Renkum", "gelderland");
+		map.put("Rheden", "gelderland");
+		map.put("Rozendaal", "gelderland");
+		map.put("Scherpenzeel", "gelderland");
+		map.put("Tiel", "gelderland");
+		map.put("Voorst", "gelderland");
+		map.put("Wageningen", "gelderland");
+		map.put("West Betuwe", "gelderland");
+		map.put("West Maas en Waal", "gelderland");
+		map.put("Westervoort", "gelderland");
+		map.put("Wijchen", "gelderland");
+		map.put("Winterswijk", "gelderland");
+
+		//Zuid-Holland
+		map.put("Alblasserdam","zuidholland");
+		map.put("Albrandswaard","zuidholland");
+		map.put("Alphen aan den Rijn","zuidholland");
+		map.put("Barendrecht","zuidholland");
+		map.put("Bodegraven-Reeuwijk","zuidholland");
+		map.put("Brielle","zuidholland");
+		map.put("Capelle aan den IJssel","zuidholland");
+		map.put("Delft","zuidholland");
+		map.put("Den Haag","zuidholland");
+		map.put("Dordrecht","zuidholland");
+		map.put("'s-Gravenhage","zuidholland");
+		map.put("Goeree-Overflakkee","zuidholland");
+		map.put("Gorinchem","zuidholland");
+		map.put("Gouda","zuidholland");
+		map.put("Hardinxveld-Giessendam", "zuidholland");
+		map.put("Hellevoetsluis","zuidholland");
+		map.put("Hendrik-Ido-Ambacht","zuidholland");
+		map.put("Hillegom","zuidholland");
+		map.put("Hoeksche Waard","zuidholland");
+		map.put("Kaag en Braassem","zuidholland");
+		map.put("Katwijk","zuidholland");
+		map.put("Krimpen aan den IJssel","zuidholland");
+		map.put("Krimpenerwaard","zuidholland");
+		map.put("Lansingerland","zuidholland");
+		map.put("Leiden","zuidholland");
+		map.put("Leiderdorp","zuidholland");
+		map.put("Leidschendam-Voorburg","zuidholland");
+		map.put("Lisse","zuidholland");
+		map.put("Maassluis","zuidholland");
+		map.put("Midden-Delfland","zuidholland");
+		map.put("Molenlanden","zuidholland");
+		map.put("Nieuwkoop","zuidholland");
+		map.put("Nissewaard","zuidholland");
+		map.put("Noordwijk","zuidholland");
+		map.put("Oegstgeest","zuidholland");
+		map.put("Papendrecht","zuidholland");
+		map.put("Pijnacker-Nootdorp","zuidholland");
+		map.put("Ridderkerk","zuidholland");
+		map.put("Rijswijk","zuidholland");
+		map.put("Rotterdam","zuidholland");
+		map.put("Schiedam","zuidholland");
+		map.put("Sliedrecht","zuidholland");
+		map.put("Teylingen","zuidholland");
+		map.put("Vlaardingen","zuidholland");
+		map.put("Voorschoten","zuidholland");
+		map.put("Waddinxveen","zuidholland");
+		map.put("Wassenaar","zuidholland");
+		map.put("Westland","zuidholland");
+		map.put("Westvoorne","zuidholland");
+		map.put("Zoetermeer","zuidholland");
+		map.put("Zoeterwoude","zuidholland");
+		map.put("Zuidplas","zuidholland");
+		map.put("Zwijndrecht","zuidholland");
+
+		//Groningen
+		map.put("Appingedam", "groningen");
+		map.put("Delfzijl", "groningen");
+		map.put("Groningen", "groningen");
+		map.put("Het Hogeland", "groningen");
+		map.put("Loppersum", "groningen");
+		map.put("Midden-Groningen", "groningen");
+		map.put("Oldambt", "groningen");
+		map.put("Pekela", "groningen");
+		map.put("Stadskanaal", "groningen");
+		map.put("Veendam", "groningen");
+		map.put("Westerkwartier", "groningen");
+		map.put("Westerwolde", "groningen");
+
+		//Limburg
+		map.put("Beek", "limburg");
+		map.put("Beekdaelen", "limburg");
+		map.put("Beesel", "limburg");
+		map.put("Bergen (L.)", "limburg");
+		map.put("Brunssum", "limburg");
+		map.put("Echt-Susteren", "limburg");
+		map.put("Eijsden-Margraten", "limburg");
+		map.put("Gennep", "limburg");
+		map.put("Gulpen-Wittem", "limburg");
+		map.put("Heerlen", "limburg");
+		map.put("Horst aan de Maas", "limburg");
+		map.put("Kerkrade", "limburg");
+		map.put("Landgraaf", "limburg");
+		map.put("Leudal", "limburg");
+		map.put("Maasgouw", "limburg");
+		map.put("Maastricht", "limburg");
+		map.put("Meerssen", "limburg");
+		map.put("Mook en Middelaar", "limburg");
+		map.put("Nederweert", "limburg");
+		map.put("Peel en Maas", "limburg");
+		map.put("Roerdalen", "limburg");
+		map.put("Roermond", "limburg");
+		map.put("Simpelveld", "limburg");
+		map.put("Sittard-Geleen", "limburg");
+		map.put("Stein", "limburg");
+		map.put("Vaals", "limburg");
+		map.put("Valkenburg aan de Geul", "limburg");
+		map.put("Venlo", "limburg");
+		map.put("Venray", "limburg");
+		map.put("Voerendaal", "limburg");
+		map.put("Weert", "limburg");
+
+		//Noord-Brabant
+		map.put("Alphen-Chaam", "noord-brabant");
+		map.put("Altena", "noord-brabant");
+		map.put("Asten", "noord-brabant");
+		map.put("Baarle-Nassau", "noord-brabant");
+		map.put("Bergeijk", "noord-brabant");
+		map.put("Bergen op Zoom", "noord-brabant");
+		map.put("Bernheze", "noord-brabant");
+		map.put("Best", "noord-brabant");
+		map.put("Bladel", "noord-brabant");
+		map.put("Boekel", "noord-brabant");
+		map.put("Boxmeer", "noord-brabant");
+		map.put("Boxtel", "noord-brabant");
+		map.put("Breda", "noord-brabant");
+		map.put("Cranendonck", "noord-brabant");
+		map.put("Cuijk", "noord-brabant");
+		map.put("Deurne", "noord-brabant");
+		map.put("Dongen", "noord-brabant");
+		map.put("Drimmelen", "noord-brabant");
+		map.put("Eersel", "noord-brabant");
+		map.put("Eindhoven", "noord-brabant");
+		map.put("Etten-Leur", "noord-brabant");
+		map.put("Geertruidenberg", "noord-brabant");
+		map.put("Geldrop-Mierlo", "noord-brabant");
+		map.put("Gemert-Bakel", "noord-brabant");
+		map.put("Gilze en Rijen", "noord-brabant");
+		map.put("Goirle", "noord-brabant");
+		map.put("Grave", "noord-brabant");
+		map.put("Haaren", "noord-brabant");
+		map.put("Halderberge", "noord-brabant");
+		map.put("Heeze-Leende", "noord-brabant");
+		map.put("Helmond", "noord-brabant");
+		map.put("'s-Hertogenbosch", "noord-brabant");
+		map.put("Heusden", "noord-brabant");
+		map.put("Hilvarenbeek", "noord-brabant");
+		map.put("Laarbeek", "noord-brabant");
+		map.put("Landerd", "noord-brabant");
+		map.put("Loon op Zand", "noord-brabant");
+		map.put("Meierijstad", "noord-brabant");
+		map.put("Mill en Sint Hubert", "noord-brabant");
+		map.put("Moerdijk", "noord-brabant");
+		map.put("Nuenen c.a.", "noord-brabant");
+		map.put("Oirschot", "noord-brabant");
+		map.put("Oisterwijk", "noord-brabant");
+		map.put("Oosterhout", "noord-brabant");
+		map.put("Oss", "noord-brabant");
+		map.put("Reusel-De Mierden", "noord-brabant");
+		map.put("Roosendaal", "noord-brabant");
+		map.put("Rucphen", "noord-brabant");
+		map.put("Sint Anthonis", "noord-brabant");
+		map.put("Sint-Michielsgestel", "noord-brabant");
+		map.put("Someren", "noord-brabant");
+		map.put("Son en Breugel", "noord-brabant");
+		map.put("Steenbergen", "noord-brabant");
+		map.put("Tilburg", "noord-brabant");
+		map.put("Uden", "noord-brabant");
+		map.put("Valkenswaard", "noord-brabant");
+		map.put("Veldhoven", "noord-brabant");
+		map.put("Vught", "noord-brabant");
+		map.put("Waalre", "noord-brabant");
+		map.put("Waalwijk", "noord-brabant");
+		map.put("Woensdrecht", "noord-brabant");
+		map.put("Zundert", "noord-brabant");
+		map.put("Nuenen, Gerwen en Nederwetten", "noord-brabant");
+
+		//overijssel
+		map.put("Almelo", "overijssel");
+		map.put("Borne", "overijssel");
+		map.put("Dalfsen", "overijssel");
+		map.put("Deventer", "overijssel");
+		map.put("Dinkelland", "overijssel");
+		map.put("Enschede", "overijssel");
+		map.put("Haaksbergen", "overijssel");
+		map.put("Hardenberg", "overijssel");
+		map.put("Hellendoorn", "overijssel");
+		map.put("Hengelo", "overijssel");
+		map.put("Hof van Twente", "overijssel");
+		map.put("Kampen", "overijssel");
+		map.put("Losser", "overijssel");
+		map.put("Oldenzaal", "overijssel");
+		map.put("Olst-Wijhe", "overijssel");
+		map.put("Ommen", "overijssel");
+		map.put("Raalte", "overijssel");
+		map.put("Rijssen-Holten", "overijssel");
+		map.put("Staphorst", "overijssel");
+		map.put("Steenwijkerland", "overijssel");
+		map.put("Tubbergen", "overijssel");
+		map.put("Twenterand", "overijssel");
+		map.put("Wierden", "overijssel");
+		map.put("Zwartewaterland", "overijssel");
+		map.put("Zwolle", "overijssel");
+
+		//Zeeland
+		map.put("Borsele", "zeeland");
+		map.put("Goes", "zeeland");
+		map.put("Hulst", "zeeland");
+		map.put("Kapelle", "zeeland");
+		map.put("Middelburg", "zeeland");
+		map.put("Noord-Beveland", "zeeland");
+		map.put("Reimerswaal", "zeeland");
+		map.put("Schouwen-Duiveland", "zeeland");
+		map.put("Sluis", "zeeland");
+		map.put("Terneuzen", "zeeland");
+		map.put("Tholen", "zeeland");
+		map.put("Veere", "zeeland");
+		map.put("Vlissingen", "zeeland");
+		map.put("Zaltbommel", "gelderland");
+		map.put("Zevenaar", "gelderland");
+		map.put("Zutphen", "gelderland");
+
+		// Noord-Holland
+		map.put("Aalsmeer", "noordholland");
+		map.put("Alkmaar", "noordholland");
+		map.put("Amstelveen", "noordholland");
+		map.put("Amsterdam", "noordholland");
+		map.put("Beemster", "noordholland");
+		map.put("Bergen (NH.)", "noordholland");
+		map.put("Beverwijk", "noordholland");
+		map.put("Blaricum", "noordholland");
+		map.put("Bloemendaal", "noordholland");
+		map.put("Castricum", "noordholland");
+		map.put("Den Helder", "noordholland");
+		map.put("Diemen", "noordholland");
+		map.put("Drechterland", "noordholland");
+		map.put("Edam-Volendam", "noordholland");
+		map.put("Enkhuizen", "noordholland");
+		map.put("Gooise Meren", "noordholland");
+		map.put("Haarlem", "noordholland");
+		map.put("Haarlemmermeer", "noordholland");
+		map.put("Heemskerk", "noordholland");
+		map.put("Heemstede", "noordholland");
+		map.put("Heerhugowaard", "noordholland");
+		map.put("Heiloo", "noordholland");
+		map.put("Hilversum", "noordholland");
+		map.put("Hollands Kroon", "noordholland");
+		map.put("Hoorn", "noordholland");
+		map.put("Huizen", "noordholland");
+		map.put("Koggenland", "noordholland");
+		map.put("Landsmeer", "noordholland");
+		map.put("Langedijk", "noordholland");
+		map.put("Laren", "noordholland");
+		map.put("Medemblik", "noordholland");
+		map.put("Oostzaan", "noordholland");
+		map.put("Opmeer", "noordholland");
+		map.put("Ouder-Amstel", "noordholland");
+		map.put("Purmerend", "noordholland");
+		map.put("Schagen", "noordholland");
+		map.put("Stede Broec", "noordholland");
+		map.put("Texel", "noordholland");
+		map.put("Uitgeest", "noordholland");
+		map.put("Uithoorn", "noordholland");
+		map.put("Velsen", "noordholland");
+		map.put("Waterland", "noordholland");
+		map.put("Weesp", "noordholland");
+		map.put("Wijdemeren", "noordholland");
+		map.put("Wormerland", "noordholland");
+		map.put("Zaanstad", "noordholland");
+		map.put("Zandvoort", "noordholland");
+		
+		// Utrecht
+		map.put("Amersfoort", "utrecht");
+		map.put("Baarn", "utrecht");
+		map.put("De Bilt", "utrecht");
+		map.put("Bunnik", "utrecht");
+		map.put("Bunschoten", "utrecht");
+		map.put("Eemnes", "utrecht");
+		map.put("Houten", "utrecht");
+		map.put("IJsselstein", "utrecht");
+		map.put("Leusden", "utrecht");
+		map.put("Lopik", "utrecht");
+		map.put("Montfoort", "utrecht");
+		map.put("Nieuwegein", "utrecht");
+		map.put("Oudewater", "utrecht");
+		map.put("Renswoude", "utrecht");
+		map.put("Rhenen", "utrecht");
+		map.put("De Ronde Venen", "utrecht");
+		map.put("Soest", "utrecht");
+		map.put("Stichtse Vecht", "utrecht");
+		map.put("Utrecht", "utrecht");
+		map.put("Utrechtse Heuvelrug", "utrecht");
+		map.put("Veenendaal", "utrecht");
+		map.put("Vijfheerenlanden", "utrecht");
+		map.put("Wijk bij Duurstede", "utrecht");
+		map.put("Woerden", "utrecht");
+		map.put("Woudenberg", "utrecht");
+		map.put("Zeist", "utrecht");
+	}
+
+	public Location getCouncilLocation(String councilCode)
+	{
+		return map.get(councilCode);
+	}
+}
